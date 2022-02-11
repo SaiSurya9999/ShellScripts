469fbf556cf5e2330dfc2c653613d2a4879f2c2a
tsaisurya@gmail.com <tsaisurya@gmail.com>
2022-02-11 17:34:19 +0530

    Updated the process of kubernetes.

---
+++ kubedum_master_node.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Run this script only on master
+# ****************Centos Installation setup of Kubernetes using Kubedam Method
+###Add yum-utils, if not installed already
+yum install yum-utils -y
+
+###Add Docker repository.
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+###Install Docker CE.
+yum update && yum install docker-ce-18.06.2.ce -y
+
+###Create /etc/docker directory.
+mkdir /etc/docker
+
+###Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+###Restart Docker
+systemctl daemon-reload
+systemctl enable docker.service
+systemctl restart docker
+
+# Disable SE Linux
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=disable/' /etc/selinux/config
+
+# Disable Swap
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+# Disable Firewall
+systemctl disable firewalld
+systemctl stop firewalld
+
+
+# Update IPT Tables
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+# Install kubelet/kubeadm/kubectl
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+systemctl enable --now kubelet
+
+
+# **************Master Commands****************
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir $HOME/.kube
+cp /etc/kubernetes/admin.conf $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+kubectl get cs
